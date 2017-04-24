8ad09807d33fa355f9bddc5faf62563199d9a77f
leenoad1991 <leenoad1991>
2017-04-24 17:27:26 +0100

    Map sorted

--- deployment-starter/public/js/client.js
+++ deployment-starter/public/js/client.js
@@ -1,1 +1,16 @@
-"use strict";console.log("hello world");
+console.log("map!");
+
+
+$(init);
+
+function init() {
+initMap();
+}
+
+  function initMap() {
+    var Uluru = {lat: 37.4786298, lng: -125.6627441};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: Uluru
+    });
+  }
--- deployment-starter/src/js/client.js
+++ deployment-starter/src/js/client.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+console.log('map')
+
+$(init);
+
+function init() {
+
+
+  function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: Uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+      });
+    }
