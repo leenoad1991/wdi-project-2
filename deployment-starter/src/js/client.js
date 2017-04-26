80660e7f7570a019b35e3e24824d57369487adff
leenoad1991 <leenoad1991>
2017-04-26 17:29:01 +0100

    added a few bits

--- deployment-starter/src/js/client.js
+++ deployment-starter/src/js/client.js
@@ -1,21 +1,24 @@
-// 1a5dcda526d0e910cf224214364a0669
-// ab491f2fcec88785
-
 const google = google;
+
+$(init);
 
 function init() {
   initMap();
+  const video = document.getElementById('video');
+  video.currentTime = 10;
+  video.play();
 }
 
 function initMap() {
   var California = {lat: 37.4786298, lng: -125.6627441};
   const mapCanvas = document.getElementById('map');
+  console.log('map');
   console.log(mapCanvas);
   var map = new google.maps.Map(mapCanvas, {
     zoom: 5,
     center: California
-    // map.setZoom(17);
-    // map.panTo(California);
+    // zoom: 5,
+    // map.panTo (California)
   });
   addMarkers(map);
 }
@@ -23,13 +26,84 @@ function initMap() {
 function addMarkers(map) {
   $.get('http://localhost:3000/spots')
   .done(spots => {
-    // console.log(spots);
     spots.forEach(spot => {
-      // console.log(spot.spotId);
       const location = {lat: spot.lat, lng: spot.lng};
       var marker = new google.maps.Marker({
         position: location,
-        map: map
+        map: map,
+        style: [
+          {
+            'featureType': 'road',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'transit',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'administrative.province',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'poi.park',
+            'elementType': 'geometry',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'water',
+            'stylers': [
+              {
+                'color': '#004b76'
+              }
+            ]
+          },
+          {
+            'featureType': 'landscape.natural',
+            'stylers': [
+              {
+                'visibility': 'on'
+              },
+              {
+                'color': '#fff6cb'
+              }
+            ]
+          },
+          {
+            'featureType': 'administrative.country',
+            'elementType': 'geometry.stroke',
+            'stylers': [
+              {
+                'visibility': 'on'
+              },
+              {
+                'color': '#7f7d7a'
+              },
+              {
+                'lightness': 10
+              },
+              {
+                'weight': 1
+              }
+            ]
+          }
+        ]
+
       });
 
       const formattedCounty = spot.countyName.toLowerCase().split(' ').join('-');
@@ -39,49 +113,30 @@ function addMarkers(map) {
       .done(data => {
         spot.celcius = data.celcius;
 
-        // var contentString = '<div id="content">'+
-        // `<h1 id="firstHeading" class="firstHeading">${spot.name}</h1>`+
-        // `<h2>Temperature: ${spot.celcius}</h2>`+
-        // // '<button type="button">Surfed here? Add a comment...!</button>'+
-        // <a href="/spots/<%= spot.id %>/comments">Post a Comment</a>
-        // '<div id="bodyContent">'+
-        // '</div>'+
-        // '</div>';
-
         const contentString = `
         <div id="content">
-          <h1 id="firstHeading" class="firstHeading">${spot.name}</h1>
-          <h2>Temperature: ${spot.celcius}</h2>
-          <a href="/spots/${spot._id}">View Spot</a>
-          <div id="bodyContent">
-          </div>
+        <h1 id="firstHeading" class="firstHeading">${spot.name}</h1>
+        <h2>Temperature: ${spot.celcius}</h2>
+        <a href="/spots/${spot._id}">View Spot</a>
+        <div id="bodyContent">
+        </div>
         </div>
         `;
-
-
 
         var infowindow = new google.maps.InfoWindow({
           content: contentString
         });
 
-        // console.log('clicked');
         marker.addListener('click', function() {
           infowindow.open(map, marker);
         });
       });
-
-      // $
-      // .get(`http://api.spitcast.com/api/county/tide/${formattedCounty}/`)
-      // .done(data => {
-      //   console.log(data);
-      // });
-      // //
-
     });
-
   });
 }
 
-$(init);
-
-//create link in box that allows users to add comment on new page.
+//sort markers on map.
+//nav bar - space evenly and place in the middle
+//sort out 'add a spot'
+//Google fonts.
+//Sort pan.to out.
